ae2772fa82ff7a359d3322d32b6cd3865c87e7e4
Córdova Carlos <ccordova.zosma@gmail.com>
2020-11-04 17:19:00 -0500

    Schema Upc

--- preguntas.js
+++ preguntas.js
@@ -1,16 +1,16 @@
 //Captura la etiqueta que contiene el nombre de la Carrera
 const carrera = document.getElementById("nombreCarrera");
 const carreraNombre = carrera.innerHTML;
-// Establece las preguntas de manera manual
 // Validar URL
-var URLdomain = window.location.pathname;
-// var URLdomain = window.location.host;
-if (URLdomain == "/schemaUpc/page2.html") {
+var URLdomain = window.location.host;
+if (URLdomain == "pregrado.upc.edu.pe") {
+  //Definir preguntas de Pregrado
   var subPreguntas = [
     `¿Es una prueba de ${carreraNombre}?`,
     `¿Es una segunda prueba de${carreraNombre}?`,
   ];
-} else {
+} else if (URLdomain == "epe.upc.edu.pe") {
+  //Definir preguntas de EPE
   var subPreguntas = [
     `¿Qué cursos lleva ${carreraNombre}? prueba`,
     `¿Donde estudiar ${carreraNombre}?`,
